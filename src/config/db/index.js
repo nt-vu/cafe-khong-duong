d7175bba994460561f74cbb7255bde7d354aceda
ntvu <tuanvu270502@gmail.com>
2021-08-25 23:38:18 +0700

    First-news

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-const url = process.env.MONGODB_URI ||'mongodb+srv://tuanvu02:tuanvu02@cluster0.gjwyy.mongodb.net/Hapi-blog-post'
+const mg_url = process.env.MONGODB_URI ||'mongodb+srv://tuanvu02:tuanvu02@cluster0.gjwyy.mongodb.net/Hapi-blog-post'
 
 async function connect() {
     try {  
-        await mongoose.connect(url, {
+        await mongoose.connect(mg_url, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
