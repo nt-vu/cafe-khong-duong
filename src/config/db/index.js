d01289fc38e699cce48148f2d30fa89bc20156fb
ntvu <tuanvu270502@gmail.com>
2021-08-25 21:07:15 +0700

    Alright

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 async function connect() {
     try {
         
-        await mongoose.connect('mongodb+srv://tuanvu02:tuanvu02@cluster0.gjwyy.mongodb.net/Hapi-blog-post', {
+        await mongoose.connect('mongodb+srv://tuanvu02:tuanvu02@cluster0.gjwyy.mongodb.net/Hapi-blog-post?retryWrites=true&w=majority', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
